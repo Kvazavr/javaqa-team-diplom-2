6846b9ceb61d00fe1bc5367c01142e57e6f25dbf
Elena Nekrasova <kvazavr@yandex.ru>
2023-04-05 16:19:10 +0400

    Added tests

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -17,4 +17,56 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
+
+    @Test
+    public void balanceShouldDecreaseByAmount() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                -5_000,
+                15
+        );
+
+        account.pay(12_000);
+
+        Assertions.assertEquals(5_000, account.getBalance());
+    }
+
+    @Test
+    public void rateCalculationWithPositiveBalanceShouldBeZero() {
+        CreditAccount account = new CreditAccount(
+                200,
+                -5_000,
+                15
+        );
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test
+    public void balanceLessThanCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                50,
+                100,
+                15
+        );
+
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> account.getBalance()
+        );
+    }
+
+    @Test
+    public void creditLimitShouldBePositive() {
+        CreditAccount account = new CreditAccount(
+                50,
+                -100,
+                15
+        );
+
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> account.getCreditLimit()
+        );
+    }
 }
