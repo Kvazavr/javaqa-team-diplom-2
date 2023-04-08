daafe60aefd2fcc5c4f506c3feab6279e9844c81
i.shatunova <ivashatunova@gmail.com>
2023-04-08 11:35:27 +0300

    Fix add logic, close #15

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -79,7 +79,7 @@ public class CreditAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        balance = amount;
+        balance += amount;
         return true;
     }
 
